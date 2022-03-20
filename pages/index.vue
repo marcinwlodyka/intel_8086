ae5a5fb0cce39685156450ad8f0d960826290a4b
unknown <Marcin.Wlodyka@microsoft.wsei.edu.pl>
2022-03-20 12:43:41 +0100

    start

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul v-for="(item, i) in names" :key="i">
+      <li class="flex flex-row ">
+        <div class="flex m-1 items-center">
+          <div class="w-10 text-center h-min">{{ item + 'H' }}</div>
+          <input type="text">
+        </div>
+        <div class="flex m-1 items-center">
+          <div class="w-10 text-center h-min">{{ item + 'L' }}</div>
+          <input type="text">
+        </div>
+      </li>
+    </ul>
+    
+    <button @click="test">test</button>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage'
-})
+<script>
+export default {
+  data() {
+    return {
+      names: [ 'A', 'B', 'C', 'D' ],
+      inputs: [6]
+    }
+  },
+  methods: {
+    test() {
+      this.inputs[1] = 10;
+      console.log(this.inputs[1]);
+    }
+  }
+}
 </script>
+
+<style scoped>
+input {
+  border: black solid 1px;
+  border-radius: 1rem;
+  padding: 0.25rem 1rem;
+}
+</style>
