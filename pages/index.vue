7827fb8a2b6c4bc5307173ddce0cea71c495263a
dead-playmod <playmod.pl@gmail.com>
2022-04-03 03:06:22 +0200

    changed inputs

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-  <div>
-    <ul v-for="(item, i) in names" :key="i">
-      <li class="flex flex-row ">
-        <div class="flex m-1 items-center">
-          <div class="w-10 text-center h-min">{{ item + 'H' }}</div>
-          <input type="text">
-        </div>
-        <div class="flex m-1 items-center">
-          <div class="w-10 text-center h-min">{{ item + 'L' }}</div>
-          <input type="text">
-        </div>
-      </li>
-    </ul>
-    
-    <button @click="test">test</button>
+  <div class="h-screen">
+    <form 
+      @submit.prevent=""
+      class="flex flex-row items-center w-1/2 h-1/2"
+    >
+      <ul class="flex flex-row flex-wrap">
+        <li 
+          v-for="(register, i) in registers"
+          :key="i"
+          class="w-1/2 my-2 flex flex-row justify-center"
+        >
+          <div class="bg-black p-0.5 rounded-full flex flex-row items-center">
+            <label 
+            :for="register.name"
+            class="uppercase mx-2 text-white"
+          >
+            {{ register.name }}
+          </label>
+          <input 
+            type="text"
+            :name="register.name"
+            :id="register.name"
+            maxlength="2"
+            autocomplete="off"
+            v-model="register.value"
+          >
+          </div>
+        </li>
+      </ul>
+
+      <div class="flex flex-col px-10">
+        <input
+          type="submit"
+          value="Save"
+          class="cursor-pointer"
+        >
+        <button>Reset</button>
+        <button>Random</button>
+      </div>
+    </form>    
   </div>
 </template>
 
@@ -21,23 +46,60 @@
 export default {
   data() {
     return {
-      names: [ 'A', 'B', 'C', 'D' ],
-      inputs: [6]
+      registers: [
+        {
+          name: 'ah',
+          value: ''
+        },
+        {
+          name: 'al',
+          value: ''
+        },
+        {
+          name: 'bh',
+          value: ''
+        },
+        {
+          name: 'bl',
+          value: ''
+        },
+        {
+          name: 'ch',
+          value: ''
+        },
+        {
+          name: 'cl',
+          value: ''
+        },
+        {
+          name: 'dh',
+          value: ''
+        },
+        {
+          name: 'dl',
+          value: ''
+        }
+      ]
     }
   },
   methods: {
-    test() {
-      this.inputs[1] = 10;
-      console.log(this.inputs[1]);
-    }
+    
   }
 }
 </script>
 
 <style scoped>
-input {
+input,
+button {
+  background-color: white;
   border: black solid 1px;
   border-radius: 1rem;
   padding: 0.25rem 1rem;
+}
+
+input[type = "submit"],
+button {
+  margin: 0.25rem auto;
+  width: min-content;
 }
 </style>
